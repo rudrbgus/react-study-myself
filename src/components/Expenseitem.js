1103565d03aabb7f8c14d86d7fc361341d9fea9b
rudrbgus <rudrbgus3@gmail.com>
2023-09-05 14:43:23 +0900

    react - event  처리

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Expenseitem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from './UI/Card';
+
 const Expenseitem = ({title, price, date}) => {    
   const expenseTitle = title;
   const expensePrice = price;
@@ -10,18 +11,20 @@ const Expenseitem = ({title, price, date}) => {
   const make2digit = (text) =>{
     return text.toString().padStart(2, '0');
   };
-  
+  const clickHandler = () =>{
+    console.log('버튼 클릭함');
+  }
 
 
   return (    
-    <Card  className = "circle">
-      <div className='expense-item'>
-          <ExpenseDate date={date}/>
-          <div className='expense-item__description'>
-              <h2>{expenseTitle}</h2>
-              <div className='expense-item__price'>{formattedPrice}원</div>
-          </div>
-      </div>
+    <Card  className = "expense-item">      
+            <ExpenseDate date={date}/>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{formattedPrice}원</div>
+            </div>      
+            <button id='btn' onClick={clickHandler}>수정</button>
+            <button id='btn' onClick={e=>{console.log('삭제 클릭');}}>삭제</button>
     </Card>
   );
 }
