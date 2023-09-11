1c06e7559608543bff46753f4bad9e140a36f400
rudrbgus <rudrbgus3@gmail.com>
2023-09-11 21:51:55 +0900

    react- 나만의 공부 시작 init
    다시 리액트 공부해보겠습니다 어렵네요.. ㅠ

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import './App.css';
-import Hello from './Hello';
-import Expenseitem from './components/Expenses/Expenseitem';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+
 
 
 const App = () => {
-  //지출 항목의 대한 배열
-  const expenses = [
-    {
-      title: '바나나',
-      price: '2000',
-      date: new Date(2023, 8 - 1 , 10)
-    },
-    {
-      title: '키위',
-      price: '8000',
-      date: new Date(2023, 6 - 1, 14)
-    },
-    {
-      title: '메론',
-      price: '30000',
-      date: new Date(2023, 8 - 1, 20)
-    }
-  ];
+ 
   return (  
-    <>
-      <NewExpense />      
-      <Expenses items ={expenses}/>                            
-      
-    </>  
-    
+   
   );
 }
 
